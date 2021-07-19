f55474fc302acfeddbc0059ff0e8c95097cfc013
abdul2112 <abdulkadir.s.salad1@gmail.com>
2021-07-19 16:04:26 +0100

    user>users, room>rooms

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import mongoose from "mongoose";
-import server, { app } from "./server.js";
-import list from "express-list-endpoints";
-
-const ATLAS_URL = process.env.ATLAS_URL;
-
-if (!ATLAS_URL) throw new Error("No Atlas URL specified");
+import mongoose from 'mongoose';
+import server, { app } from './server.js';
+import list from 'express-list-endpoints';
 
 const port = process.env.PORT || 3030;
+const ATLAS_URL = process.env.ATLAS_URL;
+
+if (!ATLAS_URL) throw new Error('No Atlas URL specified');
 
 mongoose
   .connect(ATLAS_URL, {
@@ -14,11 +13,11 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("Connected to mongo");
+    console.log('Connected to mongo');
     // Listen using the httpServer -
     // listening with the express instance will start a new one!!
     server.listen(port, () => {
       console.table(list(app));
-      console.log("Server listening on port " + port);
+      console.log('Server listening on port ' + port);
     });
   });
--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
-import cors from "cors";
-import express from "express";
-import { createServer } from "http";
-import { Server } from "socket.io";
-import usersRouter from "./services/users/index.js";
-import roomsRouter from "./services/rooms/index.js";
+import cors from 'cors';
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import usersRouter from './services/users/index.js';
+import roomsRouter from './services/rooms/index.js';
 
 const app = express();
 app.use(cors());
@@ -12,8 +12,8 @@ app.use(express.json());
 const server = createServer(app);
 const io = new Server(server, { allowEIO3: true });
 
-app.use("/user", usersRouter);
-app.use("/room", roomsRouter);
+app.use('/users', usersRouter);
+app.use('/rooms', roomsRouter);
 
 export { app };
 export default server;
