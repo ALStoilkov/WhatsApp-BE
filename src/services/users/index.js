dd3fa7d5c8c0ded62f86db97234b07f09d09ea99
gregoriofracassi <gregorio.fracassi@gmail.com>
2021-07-20 13:59:48 +0200

    implemented jwt auth

--- src/services/users/index.js
+++ src/services/users/index.js
@@ -1,13 +1,38 @@
-import { Router } from "express";
-import UserModel from "../../models/User/index.js";
+import { Router } from "express"
+import UserModel from "../../models/User/index.js"
+import { JWTAuthenticate } from "../../auth/tools.js"
 
-const usersRouter = Router();
+const usersRouter = Router()
 
 usersRouter.post("/", async (req, res) => {
-  const newUser = new UserModel(req.body);
-  await newUser.save();
+  try {
+    const newUser = new UserModel(req.body)
+    const { _id } = await newUser.save()
 
-  res.status(201).send(newUser);
-});
+    res.status(201).send(_id)
+  } catch (error) {
+    console.log(error)
+    next(createError(500, "An error occurred while saving new author"))
+  }
+})
 
-export default usersRouter;
+usersRouter.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body
+    console.log(email)
+    const user = await UserModel.checkCredentials(email, password)
+
+    if (user) {
+      console.log("credentials are fine")
+      const accessToken = await JWTAuthenticate(user)
+      console.log("token", accessToken)
+      res.send({ accessToken })
+    } else {
+      next(createError(401))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+export default usersRouter
