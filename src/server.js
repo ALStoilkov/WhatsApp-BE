54117e6e10dae1231f249cbc10ba2085c69ab18c
abdul2112 <abdulkadir.s.salad1@gmail.com>
2021-07-20 15:39:51 +0100

    added simple routes to user. also created errorHandlers

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
-import cors from "cors";
-import express from "express";
-import { createServer } from "http";
-import { Server } from "socket.io";
-import usersRouter from "./services/users/index.js";
-import roomsRouter from "./services/rooms/index.js";
+import cors from 'cors';
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import usersRouter from './services/users/index.js';
+import roomsRouter from './services/rooms/index.js';
+import {
+  badRequestErrorHandler,
+  catchAllErrorHandler,
+  forbiddenErrorHandler,
+  notFoundErrorHandler,
+} from './errorHandlers.js';
 
 const app = express();
 app.use(cors());
@@ -12,10 +18,10 @@ app.use(express.json());
 const server = createServer(app);
 const io = new Server(server, { allowEIO3: true });
 
-io.on("connection", (socket) => {
-  console.log("someone connected", socket.id);
+io.on('connection', (socket) => {
+  console.log('someone connected', socket.id);
 });
-io.on("sendMessage", ({ room, message }) => {
+io.on('sendMessage', ({ room, message }) => {
   await RoomModel.findOneAndUpdate(
     { _id: room },
     {
@@ -23,11 +29,18 @@ io.on("sendMessage", ({ room, message }) => {
     }
   );
 
-  socket.to(room).emit("message", message);
+  socket.to(room).emit('message', message);
 });
 
-app.use("/users", usersRouter);
-app.use("/rooms", roomsRouter);
+app.use('/users', usersRouter);
+app.use('/rooms', roomsRouter);
+
+app.use(
+  badRequestErrorHandler,
+  forbiddenErrorHandler,
+  notFoundErrorHandler,
+  catchAllErrorHandler
+);
 
 export { app };
 export default server;
--- src/services/users/index.js
+++ src/services/users/index.js
@@ -1,13 +1,56 @@
-import { Router } from "express";
-import UserModel from "../../models/User/index.js";
+import { Router } from 'express';
+import createError from 'http-errors';
+import UserModel from '../../models/User/index.js';
 
 const usersRouter = Router();
 
-usersRouter.post("/", async (req, res) => {
+usersRouter.post('/', async (req, res) => {
   const newUser = new UserModel(req.body);
   await newUser.save();
 
   res.status(201).send(newUser);
+});
+
+usersRouter.get('/', async (req, res, next) => {
+  try {
+    const users = await UserModel.find();
+    res.status(200).send(users);
+  } catch (error) {
+    next(createError(500, { message: error.message }));
+  }
+});
+
+usersRouter.get('/:id', async (req, res, next) => {
+  try {
+    const user = await UserModel.findById(req.params.id);
+    if (!user) next(createError(404, `ID ${req.params.id} was not found`));
+    else res.status(200).send(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.put('/:id', async (req, res, next) => {
+  try {
+    const updateUser = await UserModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { runValidators: true, new: true }
+    );
+    res.status(201).send(updateUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.delete('/:id', async (req, res, next) => {
+  try {
+    const deleteUser = await UserModel.findByIdAndDelete(req.params.id);
+    if (deleteUser) res.status(201).send('Profile deleted');
+    else next(createError(400, 'Bad Request'));
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default usersRouter;
