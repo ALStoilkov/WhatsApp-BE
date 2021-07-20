3f1755fb685f56289e39764219b4da705d42b08f
ALStoilkov <antonistoilkov@gmail.com>
2021-07-20 11:14:10 +0100

    user schema added

--- src/models/User/index.js
+++ src/models/User/index.js
@@ -5,6 +5,19 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  jwt: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    default: "https://via.placeholder.com/150",
+  },
+  status: {
+    type: String,
+    enum: ["Online", "Offline"],
+    default: "Offline",
+  },
 });
 
 export default mongoose.model("User", UserSchema);
