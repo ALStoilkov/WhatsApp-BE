44915e8ebb05acc4f0cbd79ce09b764024d2b3b6
Tommy Knight <78544868+Tommy-Knight@users.noreply.github.com>
2021-07-22 13:01:44 +0100

    multer

--- src/models/User/index.js
+++ src/models/User/index.js
@@ -1,10 +1,39 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-  },
+	username: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	role: {
+		type: String,
+		required: true,
+		enum: ["Admin", "User"],
+		default: "User",
+	},
+	avatar: {
+		type: String,
+		required: true,
+		default: "https://image.flaticon.com/icons/png/512/5173/5173555.png",
+	},
+});
+
+UserSchema.pre('save', async function (next) {
+  const newUser = this;
+  const plainPw = newUser.password;
+
+  if (newUser.isModified('password')) {
+    newUser.password = await bcrypt.hash(plainPw, 10);
+  }
+  next();
 });
 
 export default mongoose.model("User", UserSchema);
--- src/services/users/index.js
+++ src/services/users/index.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
 import UserModel from "../../models/User/index.js";
+import { JWTAuthenticate } from "../../auth/tools.js";
+import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 
 const usersRouter = Router();
 
@@ -9,5 +13,33 @@ usersRouter.post("/", async (req, res) => {
 
   res.status(201).send(newUser);
 });
+
+//------------------------------------CLOUDINARY---
+
+    const cloudinaryStorage = new CloudinaryStorage({
+			cloudinary,
+			params: { folder: "whatsapp" },
+		})
+
+		const upload = multer({
+			storage: cloudinaryStorage,
+		}).single("image")
+
+		usersRouter.post("/:id/upload", upload, async (req, res, next) => {
+			try {
+				const data = await user.update(
+					{ imageUrl: req.file.path },
+					{
+						where: { _id: req.params.id },
+						returning: true,
+					}
+          )
+          console.log(data)
+				if (data[0] === 1) res.send(data[1][0])
+				else res.status(404).send("ID not found")
+			} catch (error) {
+				next(error.message)
+			}
+		})
 
 export default usersRouter;
