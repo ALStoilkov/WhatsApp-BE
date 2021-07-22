9ba62ccef151ecf6126260745a39c1b20dc0cc78
ALStoilkov <antonistoilkov@gmail.com>
2021-07-22 12:37:45 +0100

    query search totally working

--- src/services/users/index.js
+++ src/services/users/index.js
@@ -1,56 +1,74 @@
-import { Router } from "express"
-import createError from "http-errors"
-import UserModel from "../../models/User/index.js"
-import { JWTAuthenticate } from "../../auth/tools.js"
+import { Router } from "express";
+import createError from "http-errors";
+import UserModel from "../../models/User/index.js";
+import { JWTAuthenticate } from "../../auth/tools.js";
 
-const usersRouter = Router()
+const usersRouter = Router();
+
+usersRouter.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    console.log(email);
+    const user = await UserModel.checkCredentials(email, password);
+
+    if (user) {
+      console.log("credentials are fine");
+      const accessToken = await JWTAuthenticate(user);
+      console.log("token", accessToken);
+      res.send({ accessToken, userId: user._id });
+    } else {
+      next(createError(401));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 usersRouter.post("/", async (req, res) => {
   try {
-    const newUser = new UserModel(req.body)
-    const { _id } = await newUser.save()
+    const newUser = new UserModel(req.body);
+    const { _id } = await newUser.save();
 
-    res.status(201).send(_id)
+    res.status(201).send(_id);
   } catch (error) {
-    console.log(error)
-    next(createError(500, "An error occurred while saving new author"))
+    console.log(error);
+    next(createError(500, "An error occurred while saving new author"));
   }
-})
+});
 
 usersRouter.get("/", async (req, res, next) => {
   try {
-    const users = await UserModel.find()
-    res.status(200).send(users)
+    const users = await UserModel.find();
+    res.status(200).send(users);
   } catch (error) {
-    next(createError(500, { message: error.message }))
+    next(createError(500, { message: error.message }));
   }
-})
+});
 
-usersRouter.get("/:username", async (req, res, next) => {
+usersRouter.get("/search", async (req, res, next) => {
+  console.log(req.query);
+  const { username } = req.query;
   try {
-    const users = await UserModel.find(
-      { username: req.params.username },
-      { email: 0 }
-    )
+    const users = await UserModel.find({ username }, { email: 0 });
 
     if (users) {
-      res.status(200).send(users)
+      res.status(200).send(users);
     }
-    res.send(createError(404, "No user found"))
+    res.send(createError(404, "No user found"));
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-// usersRouter.get('/:id', async (req, res, next) => {
-//   try {
-//     const user = await UserModel.findById(req.params.id);
-//     if (!user) next(createError(404, `ID ${req.params.id} was not found`));
-//     else res.status(200).send(user);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
+usersRouter.get("/:id", async (req, res, next) => {
+  try {
+    const user = await UserModel.findById(req.params.id);
+    if (!user) next(createError(404, `ID ${req.params.id} was not found`));
+    else res.status(200).send(user);
+  } catch (error) {
+    next(error);
+  }
+});
 
 usersRouter.put("/:id", async (req, res, next) => {
   try {
@@ -58,40 +76,21 @@ usersRouter.put("/:id", async (req, res, next) => {
       req.params.id,
       req.body,
       { runValidators: true, new: true }
-    )
-    res.status(201).send(updateUser)
+    );
+    res.status(201).send(updateUser);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 usersRouter.delete("/:id", async (req, res, next) => {
   try {
-    const deleteUser = await UserModel.findByIdAndDelete(req.params.id)
-    if (deleteUser) res.status(201).send("Profile deleted")
-    else next(createError(400, "Bad Request"))
+    const deleteUser = await UserModel.findByIdAndDelete(req.params.id);
+    if (deleteUser) res.status(201).send("Profile deleted");
+    else next(createError(400, "Bad Request"));
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-usersRouter.post("/login", async (req, res, next) => {
-  try {
-    const { email, password } = req.body
-    console.log(email)
-    const user = await UserModel.checkCredentials(email, password)
-
-    if (user) {
-      console.log("credentials are fine")
-      const accessToken = await JWTAuthenticate(user)
-      console.log("token", accessToken)
-      res.send({ accessToken, userId: user._id })
-    } else {
-      next(createError(401))
-    }
-  } catch (error) {
-    next(error)
-  }
-})
-
-export default usersRouter
+export default usersRouter;
